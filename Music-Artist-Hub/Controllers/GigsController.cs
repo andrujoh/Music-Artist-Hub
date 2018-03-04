2f8e4b9bded6921f6a8b0850f8291db0803c3c11
andrujoh <andru_joh@yahoo.no>
2018-03-04 21:54:43 +0100

    Create Add a gig form

---
+++ Music-Artist-Hub/Controllers/GigsController.cs
@@ -0,0 +1,27 @@
+﻿using Music_Artist_Hub.Models;
+using Music_Artist_Hub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Music_Artist_Hub.Controllers
+{
+  public class GigsController : Controller
+  {
+    private readonly ApplicationDbContext _context;
+
+    public GigsController()
+    {
+      _context = new ApplicationDbContext();
+    }
+    
+    public ActionResult Create()
+    {
+      var viewModel = new GigFormViewModel
+      {
+        Genres = _context.Genres.ToList()
+      };
+
+      return View(viewModel);
+    }
+  }
+}
---
+++ Music-Artist-Hub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Music_Artist_Hub.Models;
+using System.Collections.Generic;
+
+namespace Music_Artist_Hub.ViewModels
+{
+  public class GigFormViewModel
+  {
+    public string Venue { get; set; }
+    public string Date { get; set; }
+    public string Time { get; set; }
+    public int Genre { get; set; }
+    public IEnumerable<Genre> Genres { get; set; }
+  }
+}
