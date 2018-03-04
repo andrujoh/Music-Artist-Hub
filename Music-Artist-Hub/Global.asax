14518405869222efd5ba1dba0d546e89163ad23e
andrujoh <andru_joh@yahoo.no>
2018-03-04 15:23:15 +0100

    Add project files.

---
+++ Music-Artist-Hub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Music_Artist_Hub.MvcApplication" Language="C#" %>
---
+++ Music-Artist-Hub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Music_Artist_Hub.Startup))]
+namespace Music_Artist_Hub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
