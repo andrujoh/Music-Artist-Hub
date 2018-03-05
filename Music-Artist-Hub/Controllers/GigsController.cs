ba30eb4eb47126410c1ae0f6967cc0d76582279f
andrujoh <andru_joh@yahoo.no>
2018-03-05 16:23:47 +0100

    Implement antiforgerytoken

--- Music-Artist-Hub/Controllers/GigsController.cs
+++ Music-Artist-Hub/Controllers/GigsController.cs
@@ -28,6 +28,7 @@ namespace Music_Artist_Hub.Controllers
 
     [Authorize]
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public ActionResult Create(GigFormViewModel viewModel)
     {
       if (!ModelState.IsValid)
