138b66c0e99cbf0d4e9c852a33a072f92fa3df98
andrujoh <andru_joh@yahoo.no>
2018-03-05 13:34:36 +0100

    Implementetd validation

--- Music-Artist-Hub/Controllers/GigsController.cs
+++ Music-Artist-Hub/Controllers/GigsController.cs
@@ -30,11 +30,15 @@ namespace Music_Artist_Hub.Controllers
     [HttpPost]
     public ActionResult Create(GigFormViewModel viewModel)
     {
-
+      if (!ModelState.IsValid)
+      {
+        viewModel.Genres = _context.Genres.ToList();
+        return View("Create", viewModel);
+      }
       var gig = new Gig
       {
         ArtistId = User.Identity.GetUserId(),
-        DateTime = viewModel.DateTime,
+        DateTime = viewModel.GetDateTime(),
         GenreId = viewModel.Genre,
         Venue = viewModel.Venue
       };
--- Music-Artist-Hub/ViewModels/GigFormViewModel.cs
+++ Music-Artist-Hub/ViewModels/GigFormViewModel.cs
@@ -1,22 +1,32 @@
 ﻿using Music_Artist_Hub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Music_Artist_Hub.ViewModels
 {
   public class GigFormViewModel
   {
+    [Required]
     public string Venue { get; set; }
+
+    [Required]
+    [FutureDate]
     public string Date { get; set; }
+
+    [Required]
+    [ValidTime]
     public string Time { get; set; }
+
+    [Required]
     public byte Genre { get; set; }
+
+
     public IEnumerable<Genre> Genres { get; set; }
-    public DateTime DateTime
+
+    public DateTime GetDateTime()
     {
-      get
-      {
-        return DateTime.Parse(string.Format($"{Date}, {Time}"));
-      }
+      return DateTime.Parse(string.Format($"{Date}, {Time}"));
     }
   }
 }
