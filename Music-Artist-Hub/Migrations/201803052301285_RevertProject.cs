ec0d33f394792ab64a6017f5d2c858be2709d557
andrujoh <andru_joh@yahoo.no>
2018-03-06 00:35:23 +0100

    Add name to artist

---
+++ Music-Artist-Hub/Migrations/201803052301285_RevertProject.cs
@@ -0,0 +1,18 @@
+namespace Music_Artist_Hub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RevertProject : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String(nullable: false, maxLength: 100));
+        }
+    }
+}
