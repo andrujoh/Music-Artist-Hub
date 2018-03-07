fd4fff3a21dc9f535f4eb87734e84567639145ec
andrujoh <andru_joh@yahoo.no>
2018-03-07 01:20:01 +0100

    Added update method

--- Music-Artist-Hub/ViewModels/GigFormViewModel.cs
+++ Music-Artist-Hub/ViewModels/GigFormViewModel.cs
@@ -1,12 +1,17 @@
-﻿using Music_Artist_Hub.Models;
+﻿using Music_Artist_Hub.Controllers;
+using Music_Artist_Hub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
 
 namespace Music_Artist_Hub.ViewModels
 {
   public class GigFormViewModel
   {
+    public int Id { get; set; }
+
     [Required]
     public string Venue { get; set; }
 
@@ -23,6 +28,21 @@ namespace Music_Artist_Hub.ViewModels
 
 
     public IEnumerable<Genre> Genres { get; set; }
+
+    public string Heading { get; set; }
+
+    public string Action
+    {
+      get
+      {
+        Expression<Func<GigsController, ActionResult>> update = (c => c.Update(this));
+
+        Expression<Func<GigsController, ActionResult>> create = (c => c.Create(this));
+        //return (Id != 0) ? "Update" : "Create";
+        var action = (Id != 0) ? update : create;
+        return (action.Body as MethodCallExpression).Method.Name;
+      }
+    }
 
     public DateTime GetDateTime()
     {
