a50fc42ca7a48912f2e6178cb6a89d826f9fec3e
andrujoh <andru_joh@yahoo.no>
2018-03-07 15:37:12 +0100

    Added notifications to db

--- Music-Artist-Hub/Controllers/Api/GigsController.cs
+++ Music-Artist-Hub/Controllers/Api/GigsController.cs
@@ -27,7 +27,31 @@ namespace Music_Artist_Hub.Controllers.Api
       {
         return NotFound();
       }
+
       gig.IsCancelled = true;
+
+      var notification = new Notification
+      {
+        DateTime = DateTime.Now,
+        Gig = gig,
+        Type = NotificationType.GigCancelled
+      };
+
+      var attendees = _context.Attendances
+        .Where(a => a.GigId == gig.Id)
+        .Select(a => a.Attendee)
+        .ToList();
+
+      foreach (var attendee in attendees)
+      {
+        var userNotification = new UserNotification
+        {
+          User = attendee,
+          Notification = notification
+        };
+        _context.UserNotifications.Add(userNotification);
+      }
+
       _context.SaveChanges();
 
       return Ok();
---
+++ Music-Artist-Hub/Models/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Music_Artist_Hub.Models
+{
+  public class Notification
+  {
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public NotificationType Type { get; set; }
+
+    // Only used when gig is updated
+    public DateTime? OriginalDateTime { get; set; }
+    public string OriginalVenue { get; set; }
+
+    [Required]
+    public Gig Gig { get; set; }
+  }
+}
